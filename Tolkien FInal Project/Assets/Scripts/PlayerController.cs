0e21f642b8585b21e2feb4959ddd93c5cd30d6a4
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-10 21:36:29 -0800

    Added player movement. The player can travel on the x and y axis.

---
+++ Tolkien FInal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 5f;
+
+    public Rigidbody2D playerRB;
+    private Vector2 input;
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        movement = new Vector3(input.x, input.y, 0);
+        PlayerWalk();
+    }
+
+    private void PlayerWalk()
+    {
+        transform.position += movement * Time.deltaTime * movementSpeed;
+    }
+}
