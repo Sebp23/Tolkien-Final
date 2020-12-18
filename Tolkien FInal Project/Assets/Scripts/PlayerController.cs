91a70eeaecdd0b41c02e46f90cd20d47a385d41f
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-17 23:26:53 -0800

    Revert "Revert "Merge branch 'main' into gameplay""
    This reverts commit f6ffe6acc0e0a17fc15a26c54140b0920e02a919.

--- Tolkien FInal Project/Assets/Scripts/PlayerController.cs
+++ Tolkien FInal Project/Assets/Scripts/PlayerController.cs
@@ -11,11 +11,13 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D playerRB;
     private Vector2 input;
     private Vector3 movement;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,48 @@ public class PlayerController : MonoBehaviour
         input.y = Input.GetAxisRaw("Vertical");
         movement = new Vector3(input.x, input.y, 0);
         PlayerWalk();
+
+        if (input.x != 0 || input.y != 0)
+            anim.SetBool("moving", true);
+        else
+            anim.SetBool("moving", false);
+
+        anim.SetFloat("x axis", input.x);
+        anim.SetFloat("y axis", input.y);
+
+        if (input.x > 0)
+        {
+            anim.SetBool("right", true);
+            anim.SetBool("left", false);
+        }
+
+        if (input.x < 0)
+        {
+            anim.SetBool("left", true);
+            anim.SetBool("right", false);
+        }
+        if (input.y > 0)
+        {
+            anim.SetBool("up", true);
+            anim.SetBool("down", false);
+        }
+        if (input.y < 0)
+        {
+            anim.SetBool("down", true);
+            anim.SetBool("up", false);
+        }
+
+        if (input.x == 0)
+        {
+            anim.SetBool("right", false);
+            anim.SetBool("left", false);
+        }
+
+        if (input.y == 0)
+        {
+            anim.SetBool("up", false);
+            anim.SetBool("up", false);
+        }
     }
 
     private void PlayerWalk()
