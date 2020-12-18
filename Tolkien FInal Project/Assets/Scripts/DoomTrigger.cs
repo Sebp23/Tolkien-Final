0ae529e34f5c4e3507bb9112fed2d1313ff3bd8a
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-17 19:41:53 -0800

    added mt doom and some cool things

---
+++ Tolkien FInal Project/Assets/Scripts/DoomTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoomTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject spacebarPrompt;
+
+    private void Start()
+    {
+        spacebarPrompt.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            spacebarPrompt.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Scene");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spacebarPrompt.SetActive(false);
+    }
+}
