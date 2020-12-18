4cb0db92050ebca2deef9fc2db4882faae3dc1f3
MaxwellJFleming <71738689+MaxwellJFleming@users.noreply.github.com>
2020-12-18 01:51:11 -0500

    frodo animation added

--- Tolkien FInal Project/Assets/Scripts/PlayerController.cs
+++ Tolkien FInal Project/Assets/Scripts/PlayerController.cs
@@ -11,11 +11,13 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D playerRB;
     private Vector2 input;
     private Vector3 movement;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,14 @@ public class PlayerController : MonoBehaviour
         input.y = Input.GetAxisRaw("Vertical");
         movement = new Vector3(input.x, input.y, 0);
         PlayerWalk();
+
+        if (input.x != 0 || input.y != 0)
+            anim.SetBool("moving", true);
+        else
+            anim.SetBool("moving", false);
+
+        anim.SetFloat("x axis", input.x);
+        anim.SetFloat("y axis", input.y);
     }
 
     private void PlayerWalk()
