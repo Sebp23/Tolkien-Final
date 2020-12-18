d44126503fc287d4c5c22acfb68b688a2b9d9b20
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-17 19:55:15 -0800

    update sprite and cool spacebar and message doom

--- Tolkien FInal Project/Assets/Scripts/DoomTrigger.cs
+++ Tolkien FInal Project/Assets/Scripts/DoomTrigger.cs
@@ -8,25 +8,31 @@ public class DoomTrigger : MonoBehaviour
     [SerializeField]
     private GameObject spacebarPrompt;
 
+    [SerializeField]
+    private GameObject doomText;
+
     private void Start()
     {
         spacebarPrompt.SetActive(false);
+        doomText.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            doomText.SetActive(true);
             spacebarPrompt.SetActive(true);
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene("Scene");
+                SceneManager.LoadScene("WinScreen");
             }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        doomText.SetActive(false);
         spacebarPrompt.SetActive(false);
     }
 }
