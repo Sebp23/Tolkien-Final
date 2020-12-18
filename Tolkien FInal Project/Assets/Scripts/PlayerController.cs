2bdc6f0dbb55e64b307c1f4890e446d7638ffb91
MaxwellJFleming <71738689+MaxwellJFleming@users.noreply.github.com>
2020-12-18 02:11:07 -0500

    animators finished

--- Tolkien FInal Project/Assets/Scripts/PlayerController.cs
+++ Tolkien FInal Project/Assets/Scripts/PlayerController.cs
@@ -35,6 +35,40 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("x axis", input.x);
         anim.SetFloat("y axis", input.y);
+
+        if (input.x > 0)
+        {
+            anim.SetBool("right", true);
+            anim.SetBool("left", false);
+        }
+
+        if (input.x < 0)
+        {
+            anim.SetBool("left", true);
+            anim.SetBool("right", false);
+        }
+        if (input.y > 0)
+        {
+            anim.SetBool("up", true);
+            anim.SetBool("down", false);
+        }
+        if (input.y < 0)
+        {
+            anim.SetBool("down", true);
+            anim.SetBool("up", false);
+        }
+
+        if (input.x == 0)
+        {
+            anim.SetBool("right", false);
+            anim.SetBool("left", false);
+        }
+
+        if (input.y == 0)
+        {
+            anim.SetBool("up", false);
+            anim.SetBool("up", false);
+        }
     }
 
     private void PlayerWalk()
