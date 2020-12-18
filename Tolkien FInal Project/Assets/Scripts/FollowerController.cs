d43809f46367512b1f8b81a7f0683c719348a973
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-17 21:25:17 -0800

    Added music, added follower, edited floor boundary

---
+++ Tolkien FInal Project/Assets/Scripts/FollowerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform objectToFollow;
+
+    [SerializeField]
+    private Vector3 offset;
+    
+    void Update()
+    {
+        transform.position = objectToFollow.position + offset;
+    }
+}
